ba9e0863440be6a96283d2ec623db6e23118af65
William <wechen114@gmail.com>
2021-08-06 16:40:09 -0400

    sdsdsdsdsd

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("theme").onclick = changeColor;   
+
+function changeColor() {
+    document.getElementById("header").style["background-color"]="#003e57"
+    document.body.style.backgroundColor = "#005070";
+ 
+}
